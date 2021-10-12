4db0b1e8276678f36f820c9f14123e95fb22e25d
kazuya kawaguchi <kawakazu80@gmail.com>
2021-10-12 09:23:18 +0900

    bump versions

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,16 +8,14 @@ module.exports = {
     jest: true
   },
   extends: [
-    'plugin:vue-libs/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:prettier/recommended',
     'prettier'
   ],
   plugins: ['@typescript-eslint'],
-  parser: 'vue-eslint-parser',
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    parser: '@typescript-eslint/parser',
     sourceType: 'module'
   },
   rules: {
