5cd1760c002a1764e6de6520692de7ec60c35f78
kazuya kawaguchi <kawakazu80@gmail.com>
2023-09-19 15:54:26 +0900

    tweaks

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,13 +2,6 @@
 
 set -xe
 
-if [[ ! -z ${PNPM_INSTALL} ]] ; then
-  # Restore all git changes
-  git restore --source=HEAD --staged --worktree -- package.json pnpm-lock.yaml
-  # Install pnpm
-  pnpm install --no-frozen-lockfile
-fi
-
 # Check edge release
 if [[ ! -z ${EDGE_RELEASE} ]] ; then
   npx jiti ./scripts/bump-edge
@@ -19,7 +12,6 @@ if [[ ! -z ${NPM_TOKEN} ]] ; then
   echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" >> ~/.npmrc
   echo "registry=https://registry.npmjs.org/" >> ~/.npmrc
   echo "always-auth=true" >> ~/.npmrc
-  npm whoami
 fi
 
 # Release packages
