9248c88ff5e440f0c9b7a8b0e7cc27bffff269ee
kazuya kawaguchi <kawakazu80@gmail.com>
2023-09-24 21:39:41 +0900

    fix: use `@intlify/utils` in `getLocale`

--- test/i18n.test.ts
+++ test/i18n.test.ts
@@ -1,44 +1,23 @@
-import { assert, beforeEach, afterEach, describe, it } from 'vitest'
+import { afterEach, describe, test, vi, expect } from 'vitest'
 import { getLocale } from '../src/i18n'
 
-let ORG_ENV = {}
-
-beforeEach(function () {
-  ORG_ENV = process.env
-  process.env = {}
+afterEach(() => {
+  vi.resetAllMocks()
 })
 
-afterEach(function () {
-  process.env = ORG_ENV
-})
-
-describe('getLocale', function () {
-  it('default', function () {
-    assert.equal(getLocale(), 'en-US')
+describe('getLocale', () => {
+  test('default value', () => {
+    vi.spyOn(process, 'env', 'get').mockReturnValue({})
+    expect(getLocale()).toBe('en-US')
   })
 
-  it('LC_ALL', function () {
-    process.env['LC_ALL'] = 'ja_JP'
-    assert.equal(getLocale(), 'ja-JP')
-  })
-
-  it('LC_MESSAGES', function () {
-    process.env['LC_MESSAGES'] = 'ja_JP'
-    assert.equal(getLocale(), 'ja-JP')
-  })
-
-  it('LANG', function () {
-    process.env['LANG'] = 'ja_JP'
-    assert.equal(getLocale(), 'ja-JP')
-  })
-
-  it('LANGUAGE', function () {
-    process.env['LANGUAGE'] = 'ja_JP'
-    assert.equal(getLocale(), 'ja-JP')
-  })
-
-  it('BCP-47', function () {
-    process.env['LC_ALL'] = 'ja-JP'
-    assert.equal(getLocale(), 'ja-JP')
+  test('basic', () => {
+    vi.spyOn(process, 'env', 'get').mockReturnValue({
+      LC_ALL: 'en-GB',
+      LC_MESSAGES: 'en-US',
+      LANG: 'ja-JP',
+      LANGUAGE: 'en'
+    })
+    expect(getLocale()).toBe('en-GB')
   })
 })
